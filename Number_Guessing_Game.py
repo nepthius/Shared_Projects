c96b35ca9459f03e5ba04e1bd219d38c034b222c
nepthius <anishdshekar@gmail.com>
2022-02-26 14:19:00 -0500

    Created the Number_Guessing_Game

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,1 +1,22 @@
+import random
+
+num = random.randrange(0,10)
+ug = -1
+t = 0
+
+while ug != num: 
+    ug = input("Guess a number between 1 and 10: ")
+    
+    if ug > num:
+        print("You guessed too high!")
+
+    if ug < num:
+        print("You guessed too low...")
+
+    t += 1
+
+print("Congrats you guessed the number, and it only took you {} tries!".format( t))
+
+
+
 
